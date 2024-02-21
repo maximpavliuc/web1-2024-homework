3ca5cac4ec5e562c981990ea7f24760b613413ad
Maxim Pavliuc <maksik.pavlyuk@mail.ru>
2024-02-21 23:20:49 +0200

    task 10

---
+++ task10/app.js
@@ -0,0 +1,11 @@
+const dividers = (number) => {
+    console.log(`Делители числа ${number}:`);
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
+            console.log(i);
+        }
+    }
+}
+
+
+dividers(20);
